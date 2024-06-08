18aee2ccbb61650926b0188f6c1cccdfcf3c485d
Jorge Galvis <jorlugaqui@gmail.com>
2024-06-07 20:13:25 -0500

    chore: split configs

--- todoapp/settings/base.py
+++ todoapp/settings/base.py
@@ -11,24 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
-
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG")
-
-ALLOWED_HOSTS = config("DJANGO_ALLOWED_HOSTS").split()
 
 
 # Application definition
@@ -76,22 +62,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "todoapp.wsgi.application"
-
-
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-
-DATABASES = {
-    "default": {
-        "ENGINE": config("DB_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": config("DB_DATABASE", BASE_DIR / "db.sqlite3"),
-        "USER": config("DB_USER", "user"),
-        "PASSWORD": config("DB_PASSWORD", "password"),
-        "HOST": config("DB_HOST", "localhost"),
-        "PORT": config("DB_PORT", "5432"),
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
