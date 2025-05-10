e90965024206ac2e20081d203b4a2b30c868adb6
Jorge Galvis <jorlugaqui@gmail.com>
2025-05-10 16:19:31 -0500

    chore: config for railway deployment

--- todoapp/settings/prod.py
+++ todoapp/settings/prod.py
@@ -11,7 +11,7 @@ SECRET_KEY = config("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG")
+DEBUG = config("DEBUG", False, cast=bool)
 
 ALLOWED_HOSTS = config("DJANGO_ALLOWED_HOSTS").split()
 
